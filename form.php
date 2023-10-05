813f7a888d7e419b89e16de7e24947efa771c3e0
Juan Sebastian Cano <113861725+Juan-SCV@users.noreply.github.com>
2023-10-05 11:38:41 -0500

    Update form.php
    changed dbname to "universidad"

--- form.php
+++ form.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
-$dbname = "Universidad"; 
+$dbname = "universidad"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
