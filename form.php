1ab4531b18779d245f7557132955b21b6a0939fe
Juan-SCV <juanse.valencia200550@gmail.com>
2023-10-05 09:10:47 -0500

    Create form

---
+++ form.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "Universidad"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $gender = $_POST["gender"];
+    $age = $_POST["age"];
+    $description = $_POST["description"];
+    $email = $_POST["email"];
+
+    $sql = "INSERT INTO students (name, gender, age, email, description)
+            VALUES ('$name', '$gender', '$age', '$email', '$description')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Data inserted successfully.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
